d2a4a519a2985175702fbe43751671d7be0fac82
Kabir Kang <kangkabir@gmail.com>
2024-10-06 14:11:52 -0400

    Download labeled and unlabeled data

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from wilds import get_dataset
 
 dataset = get_dataset(dataset="iwildcam", download=True)
+dataset_unlabeled = get_dataset(dataset="iwildcam", download=True, unlabeled=True)
