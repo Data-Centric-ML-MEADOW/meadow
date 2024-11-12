e26c8b7892d56b0a3135583d9dde4798d74fa056
Kabir Kang <kangkabir@gmail.com>
2024-11-12 10:40:34 -0500

    vit transformations

--- utils/mappings.py
+++ utils/mappings.py
@@ -6,10 +6,12 @@ from torchvision import transforms
 from torchvision.transforms import v2
 
 from models.base_pretrained import PreTrainedResNet
+from models.pretrained_vit import PreTrainedViT
 
 # a mapping from model identifier to the model class
 MODEL_MAP: dict[str, Any] = {
     "resnet": PreTrainedResNet,
+    "vit": PreTrainedViT,
 }
 
 # a model may have a default set of transformations to apply to input data
@@ -35,6 +37,29 @@ TFMS_MAP: dict[str, transforms.Compose | v2.Compose] = {
             transforms.Resize(232),
             transforms.CenterCrop(224),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    ),
+    "vit": v2.Compose(
+        [
+            v2.ToImage(),
+            v2.Resize(232),
+            v2.CenterCrop(224),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    ),
+    "vit-tfms": transforms.Compose(
+        [
+            transforms.ToTensor(),
+            # random augmentations for training more robustly
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation((-30, 30)),
+            transforms.ColorJitter(
+                brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1
+            ),
+            transforms.Resize(232),
+            transforms.CenterCrop(224),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ]
     ),
     "default": v2.Compose(
