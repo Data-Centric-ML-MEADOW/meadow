38dc2a5b2336a7c68d4a502d45106ac0d1084980
Kabir Singh Kang <kkang68@login-ice-3.pace.gatech.edu>
2024-12-04 15:36:55 -0500

    final charts

--- scripts/experimental_plots.py
+++ scripts/experimental_plots.py
@@ -6,25 +6,25 @@ data = {
     "Model": [
         "ResNet 152", "ResNet 50", "ResNet 34",
         "Snapshot Ensemble", 
-        "SE 50, 3 experts", "SE 50, 3 experts", "SE 34, 5 experts", "SE 34, 3 experts",
+        "SE 50, 5 experts", "SE 50, 3 experts", "SE 34, 5 experts", "SE 34, 3 experts",
         "Mixture of Experts (MoE)", 
-        "MoE 50, 3 experts", "MoE 34, 5 experts", "MoE 34, 3 experts",
+        "MoE 50, 5 experts", "MoE 50, 3 experts", "MoE 34, 5 experts",
         "Vision Transformer (ViT)", 
         "ViT 16 No Freeze", "ViT 16 Freeze"
     ],
-    "Test Accuracy": [
-        0.721133, 0.738193, 0.682059, None,
-        0.677526, 0.677315, 0.642495, 0.640672,
+    "Test Accuracy (%)": [
+        72.1133, 73.8193, 68.2059, None,
+        67.7526, 67.7315, 64.2495, 64.0672,
         None,
-        0.681311, 0.647613, 0.678951,
+        67.8951, 68.1311, 64.7613,
         None,
-        0.561871, 0.448202
+        56.1871, 44.8202
     ],
     "Test Recall": [
         0.272167, 0.260434, 0.223976, None,
         0.255944, 0.262979, 0.227946, 0.226629,
         None,
-        0.266920, 0.222860, 0.248945,
+        0.248945, 0.266920, 0.222860,
         None,
         0.196435, 0.098078
     ],
@@ -32,7 +32,7 @@ data = {
         0.275299, 0.266368, 0.233204, None,
         0.251981, 0.257199, 0.223529, 0.221488,
         None,
-        0.260069, 0.218446, 0.245326,
+        0.245326, 0.260069, 0.218446,
         None,
         0.169430, 0.088369
     ]
@@ -49,7 +49,7 @@ nan_color = 'white'
 # Prepare the table for Plotly
 fig = go.Figure(data=[go.Table(
     header=dict(
-        values=["<b>Model</b>", "<b>Test Accuracy</b>", "<b>Test Recall</b>", "<b>Test F1</b>"],
+        values=["<b>Model</b>", "<b>Test Accuracy (%)</b>", "<b>Test Recall</b>", "<b>Test F1</b>"],
         fill_color=header_color,
         align='center',
         font=dict(size=12, color='black')
@@ -57,7 +57,7 @@ fig = go.Figure(data=[go.Table(
     cells=dict(
         values=[
             df["Model"], 
-            df["Test Accuracy"].fillna(''),  # Replace NaN with an empty string
+            df["Test Accuracy (%)"].fillna(''),  # Replace NaN with an empty string
             df["Test Recall"].fillna(''),
             df["Test F1"].fillna('')
         ],
